d0aeefdbee56158f120ce627cced2acbaa4dde42
Junior <dirceu.junior@accenture.com>
2021-12-01 14:47:55 -0300

    modelando e requisitando um Collection Resource em GET

---
+++ src/main/java/com/drj/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,23 @@
+package com.drj.algafood.api.controller;
+
+import com.drj.algafood.domain.model.Cozinha;
+import com.drj.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return cozinhaRepository.listar();
+    }
+}
