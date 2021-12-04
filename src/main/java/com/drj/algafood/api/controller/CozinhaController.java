2665f038bff4aea2c1e556cf79253212219a69e8
Dirceu Junior <dirceu.r.junior@gmail.com>
2021-12-04 16:51:48 -0300

    add/update status http for resource null

--- src/main/java/com/drj/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/drj/algafood/api/controller/CozinhaController.java
@@ -4,7 +4,10 @@ import com.drj.algafood.api.model.CozinhasXmlWrapper;
 import com.drj.algafood.domain.model.Cozinha;
 import com.drj.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,8 +46,17 @@ public class CozinhaController {
     }
 
     @GetMapping("/{id}")
-    public Cozinha buscar(@PathVariable Long id) {
-        return cozinhaRepository.buscar(id);
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long id)  {
+        Cozinha cozinha = cozinhaRepository.buscar(id);
+        if(cozinha != null){
+            return ResponseEntity.ok(cozinha);
+        }
+        return ResponseEntity.notFound().build();
+        // return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+        // return ResponseEntity.ok(cozinha);
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
+//        return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
     }
 
 }
